b866d4906fd04c5713939b6cd6f05a0876c20f4c
Nitay Rabinovich <nitay@razorgrip.com>
2017-11-12 11:54:26 +0200

    - Fix parsing service

--- src/services/parsing.service.ts
+++ src/services/parsing.service.ts
@@ -1,15 +1,21 @@
 export class Parser {
 
     static stringfyObjectProps(obj): any {
-        for (let prop of obj) {
-            prop = JSON.stringify(prop);
+        for (const propName in obj) {
+            if (obj.hasOwnProperty(propName)) {
+                const prop = obj[propName];
+                obj[propName] = JSON.stringify(prop);
+            }
         }
         return obj;
     }
 
     static parseObjectProps(obj): any {
-        for (let prop of obj) {
-            prop = JSON.parse(prop);
+        for (const propName of obj) {
+            if (obj.hasOwnProperty(propName)) {
+                const prop = obj[propName];
+                obj[propName] = JSON.parse(prop);
+            }
         }
         return obj;
     }
